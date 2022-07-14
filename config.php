92df00880c4e84335033a77e3de8d142a89487c5
thisrishabh <106669692+thisrishabh@users.noreply.github.com>
2022-07-14 23:46:48 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_test_TLb8D2vmM0TcKb';
+$keySecret = 'qnkEn2t5gY0AFX7AZ2Yjee3A';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
